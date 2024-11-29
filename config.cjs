fb8c9612c3249c945b633ff4483cfbb4a021889c
boymoha <mohaboy431hmx@gmail.com>
2024-11-29 22:04:27 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -18,9 +18,9 @@ const config = {
   
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
-  MODE: process.env.MODE || "private",
+  MODE: process.env.MODE || "public",
   PREFIX: process.env.PREFIX || "!",
-  OWNER_NAME: process.env.OWNER_NAME || "MAARVIN ID",
+  OWNER_NAME: process.env.OWNER_NAME || "SHANUWA ID",
   SUDO_NUMBER: process.env.OWNER_NUMBER || "252686506869",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
